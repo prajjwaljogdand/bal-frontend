6e1dd8843cba0e5170ca7cde9062a72e1decc929
Prajjwal Jogdand <77375933+prajjwaljogdand@users.noreply.github.com>
2022-08-25 10:54:58 +0530

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from "./component/Navbar";
 
 function App() {
  
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
        SIH2022
       </header>
     </div>
